8f7b98144b785f27dd0d464c157bb24ef2b6eb69
richienagi <32586369+richienagi@users.noreply.github.com>
2020-05-27 15:19:42 -0700

    Update Remove Duplicates.py

--- Remove Duplicates.py
+++ Remove Duplicates.py
@@ -1,1 +1,17 @@
 
+# Remove Duplicates from Array (separate array)
+
+test = [1,1,2,4,4,4]
+output = []
+
+for i in range(0,len(test)):
+    if i == 0:
+        output.append(test[i])
+    else:
+        count = 0
+        for j in range(0,len(output)):
+            if output[j] == test[i]:
+                count = count + 1
+        if count == 0:
+            output.append(test[i])
+            
