492cd6e3a7ccac0241f1ae26e180a6ebbc7de9ee
richienagi <32586369+richienagi@users.noreply.github.com>
2020-05-28 21:07:04 -0700

    Update Move Zeros.py

--- Move Zeros.py
+++ Move Zeros.py
@@ -12,4 +12,11 @@ for i in range(0,len(inputArray)):
 
 for j in range(0,zeroCount):
     outputArray.append(0)
-    
+###########################################################
+# No additional array
+inputArray = [0,1,0,3,12]
+zeroCount = inputArray.count(0)
+
+for i in range(0,zeroCount):
+    inputArray.remove(0)
+    inputArray.append(0)
